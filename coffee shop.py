cac9f9a00149713b0e16e6353d45b39ee45261f4
Ahmed Imran <ahmed.imran3160@gmail.com>
2024-08-16 07:52:25 -0700

    coffee shop bot

---
+++ coffee shop.py
@@ -0,0 +1,10 @@
+#This is just a small practice project for function print, input and combination of variables
+print("Welcome to our Coffee shop")
+
+name = input("what is your name \n")
+print("hello " + name + ", thanks for visiting us!!!")
+menu = "Coffee, latte, black coffee, espresso, cappocino"
+print("What would you like to order " + name)
+print("here is our menu \n" + menu )
+order = input()
+print("thanks for your order " + name +" we will be preparing your " + order + " shortly")
